5998e6a321d08f8c3d884a8567f400dde9b619fe
venktesh <venkysoma1998@gmail.com>
2020-07-03 11:50:43 +0530

    added the route for cart

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(); 
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/add-to-cart','CartController@addToCart')->name('cart.add'); 
