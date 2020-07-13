37dfbc8107292551c539c958d8a17d3fb5ff16c1
venktesh <venkysoma1998@gmail.com>
2020-07-13 09:58:27 +0530

    Added the query for searching the products with unneccseary removal of the code

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -8,21 +8,22 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 
     <!-- all css here -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="assets/css/themify-icons.css">
-    <link rel="stylesheet" href="assets/css/pe-icon-7-stroke.css">
-    <link rel="stylesheet" href="assets/css/icofont.css">
-    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
-    <link rel="stylesheet" href="assets/css/bundle.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="/assets/css/animate.css">
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/assets/css/themify-icons.css">
+    <link rel="stylesheet" href="/assets/css/pe-icon-7-stroke.css">
+    <link rel="stylesheet" href="/assets/css/icofont.css">
+    <link rel="stylesheet" href="/assets/css/meanmenu.min.css">
+    <link rel="stylesheet" href="/assets/css/bundle.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/responsive.css">
+    <script src="/assets/js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
 <body>
@@ -31,7 +32,7 @@
         <![endif]-->
     <!-- header start -->
     <!--Notification Section-->
-    <div class="notification-section notification-section-padding  notification-img ptb-10">
+    {{-- <div class="notification-section notification-section-padding  notification-img ptb-10">
         <div class="container-fluid">
             <div class="notification-wrapper">
                 <div class="notification-pera-graph">
@@ -47,7 +48,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <header>
         <div class="header-top-wrapper-2 border-bottom-2">
             <div class="header-info-wrapper pl-200 pr-200">
@@ -57,13 +58,12 @@
                         <li><i class="pe-7s-mail"></i> <a href="#">company@domail.info</a></li>
                     </ul>
                 </div>
-                <div class="electronics-login-register">
+                <div class="electronics-login-register ">
                     <ul>
                         <li><a href="#"><i class="pe-7s-users"></i>My Account</a></li>
                         <li><a data-toggle="modal" data-target="#exampleCompare" href="#"><i class="pe-7s-repeat"></i>Compare</a></li>
                         <li><a href="wishlist.html"><i class="pe-7s-like"></i>Wishlist</a></li>
-                        <li><a href="#"><i class="pe-7s-flag"></i>US</a></li>
-                        <li><a class="border-none" href="#"><span>$</span>USD</a></li>
+                        <li><a href="{{ route('shops.create') }}"><i class="pe-7s-shopbag"></i>Sell on Webmall</a></li> 
                     </ul>
                 </div>
             </div>
@@ -71,13 +71,13 @@
         <div class="header-bottom pt-40 pb-30 clearfix">
             <div class="header-bottom-wrapper pr-200 pl-200">
                 <div class="logo-3">
-                    <a href="{{route('home')}}">
-                        <img src="assets/img/logo/logo-3.png" alt="">
+                    <a href="{{route('home')}}">        <!-- Redirects to the home page -->
+                        <img src="/assets/img/logo/logo-3.png" alt="">
                     </a>
                 </div>
                 <div class="categories-search-wrapper">
                     <div class="all-categories">
-                        <div class="select-wrapper">
+                        {{-- <div class="select-wrapper">
                             <select class="select">
                                 <option value="">All Categories</option>
                                 <option value="">Smartphones </option>
@@ -88,12 +88,12 @@
                                 <option value="">Lights </option>
                                 <option value="">Air conditioner</option>
                             </select>
-                        </div>
+                        </div> --}}
                     </div>
                     <div class="categories-wrapper">
-                        <form action="#">
-                            <input placeholder="Enter Your key word" type="text">
-                            <button type="button"> Search </button>
+                        <form action="{{route('products.search')}}" method="GET">
+                            <input name="query" placeholder="Enter Your key word" type="text">
+                            <button type="submit"> Search </button>
                         </form>
                     </div>
                 </div>
@@ -128,60 +128,14 @@
                         <nav id="mobile-menu-active">
                             <ul class="menu-overflow">
                                 <li><a href="#">HOME</a>
-                                    <ul>
-                                        <li><a href="index.html">Fashion</a></li>
-                                        <li><a href="index-fashion-2.html">Fashion style 2</a></li>
-                                        <li><a href="index-fruits.html">Fruits</a></li>
-                                        <li><a href="index-book.html">book</a></li>
-                                        <li><a href="index-electronics.html">electronics</a></li>
-                                        <li><a href="index-electronics-2.html">electronics style 2</a></li>
-                                        <li><a href="index-food.html">food & drink</a></li>
-                                        <li><a href="index-furniture.html">furniture</a></li>
-                                        <li><a href="index-handicraft.html">handicraft</a></li>
-                                        <li><a href="index-smart-watch.html">smart watch</a></li>
-                                        <li><a href="index-sports.html">sports</a></li>
-                                    </ul>
+    
                                 </li>
-                                <li><a href="#">pages</a>
-                                    <ul>
-                                        <li><a href="about-us.html">about us</a></li>
-                                        <li><a href="menu-list.html">menu list</a></li>
-                                        <li><a href="login.html">login</a></li>
-                                        <li><a href="register.html">register</a></li>
-                                        <li><a href="cart.html">cart page</a></li>
-                                        <li><a href="checkout.html">checkout</a></li>
-                                        <li><a href="wishlist.html">wishlist</a></li>
-                                        <li><a href="contact.html">contact</a></li>
-                                    </ul>
+                                <li><a href="#">Account</a>
+                
                                 </li>
-                                <li><a href="#">shop</a>
-                                    <ul>
-                                        <li><a href="shop-grid-2-col.html"> grid 2 column</a></li>
-                                        <li><a href="shop-grid-3-col.html"> grid 3 column</a></li>
-                                        <li><a href="shop.html">grid 4 column</a></li>
-                                        <li><a href="shop-grid-box.html">grid box style</a></li>
-                                        <li><a href="shop-list-1-col.html"> list 1 column</a></li>
-                                        <li><a href="shop-list-2-col.html">list 2 column</a></li>
-                                        <li><a href="shop-list-box.html">list box style</a></li>
-                                        <li><a href="product-details.html">tab style 1</a></li>
-                                        <li><a href="product-details-2.html">tab style 2</a></li>
-                                        <li><a href="product-details-3.html"> tab style 3</a></li>
-                                        <li><a href="product-details-4.html">sticky style</a></li>
-                                        <li><a href="product-details-5.html">sticky style 2</a></li>
-                                        <li><a href="product-details-6.html">gallery style</a></li>
-                                        <li><a href="product-details-7.html">gallery style 2</a></li>
-                                        <li><a href="product-details-8.html">fixed image style</a></li>
-                                        <li><a href="product-details-9.html">fixed image style 2</a></li>
-                                    </ul>
-                                </li>
+                                
                                 <li><a href="#">BLOG</a>
-                                    <ul>
-                                        <li><a href="blog.html">blog 3 colunm</a></li>
-                                        <li><a href="blog-2-col.html">blog 2 colunm</a></li>
-                                        <li><a href="blog-sidebar.html">blog sidebar</a></li>
-                                        <li><a href="blog-details.html">blog details</a></li>
-                                        <li><a href="blog-details-sidebar.html">blog details 2</a></li>
-                                    </ul>
+                        
                                 </li>
                                 <li><a href="contact.html"> Contact  </a></li>
                             </ul>
@@ -491,7 +445,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="quick-view-list nav" role="tablist">
+                        {{-- <div class="quick-view-list nav" role="tablist">
                             <a class="active" href="#modal1" data-toggle="tab" role="tab">
                                 <img src="assets/img/quick-view/s1.jpg" alt="">
                             </a>
@@ -501,7 +455,7 @@
                             <a href="#modal3" data-toggle="tab" role="tab">
                                 <img src="assets/img/quick-view/s3.jpg" alt="">
                             </a>
-                        </div>
+                        </div> --}}
                     </div>
                     <div class="qwick-view-right">
                         <div class="qwick-view-content">
@@ -565,18 +519,18 @@
 
 
     <!-- all js here -->
-    <script src="assets/js/vendor/jquery-1.12.0.min.js"></script>
-    <script src="assets/js/popper.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/isotope.pkgd.min.js"></script>
-    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
-    <script src="assets/js/jquery.counterup.min.js"></script>
-    <script src="assets/js/waypoints.min.js"></script>
-    <script src="assets/js/ajax-mail.js"></script>
-    <script src="assets/js/owl.carousel.min.js"></script>
-    <script src="assets/js/plugins.js"></script>
-    <script src="assets/js/main.js"></script>
+    <script src="/assets/js/vendor/jquery-1.12.0.min.js"></script>
+    <script src="/assets/js/popper.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="/assets/js/isotope.pkgd.min.js"></script>
+    <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="/assets/js/jquery.counterup.min.js"></script>
+    <script src="/assets/js/waypoints.min.js"></script>
+    <script src="/assets/js/ajax-mail.js"></script>
+    <script src="/assets/js/owl.carousel.min.js"></script>
+    <script src="/assets/js/plugins.js"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
 
 </html>
