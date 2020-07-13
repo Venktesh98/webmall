61475df6b7833c14f3033a184b9e46fe5c6ebd36
venktesh <venkysoma1998@gmail.com>
2020-07-13 10:56:34 +0530

    Applied the coupon code functionality on cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,18 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Cart;
+use App\Coupon;
 use App\Product;
+use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    // Shows the Items in the cart
-    public function index()
-    {
-        $cartItems = \Cart::session(auth()->user()->id)->getContent();
-        return view('cart.index',compact('cartItems'));
-    }
-
     // Add Items in the cart
     public function addToCart(Product $productid)
     {
@@ -35,6 +30,13 @@ class CartController extends Controller
 
         // return redirect('/cart');
         return redirect()->route('cart.index');
+    }
+    
+    // Shows the Items in the cart
+    public function index()
+    {
+        $cartItems = \Cart::session(auth()->user()->id)->getContent();
+        return view('cart.index',compact('cartItems'));
     }
     
     // Updates the cart
@@ -66,4 +68,27 @@ class CartController extends Controller
         return view('cart.checkout');
     }
 
+    public function applyCoupon()
+    {
+        $couponCode = request('coupon_code');
+
+        // gets from db.
+        $couponData = Coupon::where('code',$couponCode)->first();
+
+        if(!$couponData){
+            return back()->withMessage('Sorry!,coupon does not exists');
+        }
+        // coupon logic
+        $condition = new \Darryldecode\Cart\CartCondition(array(
+            'name' => $couponData->name,
+            'type' => $couponData->type,
+            'target' => 'total', // this condition will be applied to cart's total when getTotal() is called.
+            'value' => $couponData->value,
+        ));
+
+        $userId = auth()->id();
+        Cart::session($userId)->condition($condition); // for a speicifc user's cart
+
+        return back()->withMessage('Coupon Applied');
+    }
 }
